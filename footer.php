b6fbca3b7aa0ec44bf46a7390f0d9742421bf34c
VM-LXZ15268 <svantje.lilienthal@tib.eu>
2019-02-20 15:28:44 +0100

    changed to tib url

--- footer.php
+++ footer.php
@@ -4,12 +4,7 @@ i guess there will be more later on :)
 -->
 
 <footer class="footer">
-
-
-
-      <a href="/info/kontakt/">Kontakt</a>&nbsp;|&nbsp;<a href="/info/datenschutz/">Datenschutz</a>&nbsp;|&nbsp;<a href="/info/impressum/">Impressum</a>
-
-
+      <a href="https://labs.tib.eu/info/kontakt/">Kontakt</a>&nbsp;|&nbsp;<a href="https://labs.tib.eu/info/datenschutz/">Datenschutz</a>&nbsp;|&nbsp;<a href="https://labs.tib.eu/info/impressum/">Impressum</a>
 </footer>
   </body>
 </html>
