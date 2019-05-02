f44c2c0152a4c2fc7e3d40279bc0b6811c0e5c20
Svantje Lilienthal <lilients@users.noreply.github.com>
2019-05-02 18:57:33 +0200

    changed language in footer to english

--- includes/footer.php
+++ includes/footer.php
@@ -3,7 +3,9 @@ includes/footer.php
 -->
     <footer class="footer">
       <div class="container">
-       <a href="https://labs.tib.eu/info/kontakt/">Kontakt</a>&nbsp;|&nbsp;<a href="https://labs.tib.eu/info/datenschutz/">Datenschutz</a>&nbsp;|&nbsp;<a href="https://labs.tib.eu/info/impressum/">Impressum</a>
+       <a href="https://labs.tib.eu/info/en/contact/">Contact</a>&nbsp;|&nbsp;
+       <a href="https://www.tib.eu/en/service/data-protection/">Data protection</a>&nbsp;|&nbsp;
+       <a href="https://labs.tib.eu/info/en/imprint/">Imprint</a>
       </div>
     </footer>
   </body>
