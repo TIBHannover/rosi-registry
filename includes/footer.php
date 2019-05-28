3a893a8685b7593f81a8327f912f36ffeb33ea36
Svantje Lilienthal <lilients@users.noreply.github.com>
2019-05-28 17:48:26 +0200

    replaced link to contact page

--- includes/footer.php
+++ includes/footer.php
@@ -3,7 +3,7 @@ includes/footer.php
 -->
     <footer class="footer">
       <div class="container">
-       <a href="https://labs.tib.eu/info/en/contact/">Contact</a>&nbsp;|&nbsp;
+       <a href="mailto:rosi.project(at)tib.eu">Contact</a>&nbsp;|&nbsp;
        <a href="https://www.tib.eu/en/service/data-protection/">Data protection</a>&nbsp;|&nbsp;
        <a href="https://labs.tib.eu/info/en/imprint/">Imprint</a>&nbsp;|&nbsp;
        <a href="https://github.com/TIBHannover/rosi-registry">Code (GitHub)</a>
